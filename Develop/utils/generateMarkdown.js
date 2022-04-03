96ab9bb10d2056dfde46e2f4605a4329704a6421
colinc27 <30709049+colinc27@users.noreply.github.com>
2022-04-03 16:09:12 -0700

    Generate Read me with information from the

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,20 +1,31 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
-
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+//function renderLicenseBadge(license) {}
 
 // TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
-
-`;
+function generateMarkdown(readmeData) {
+  return `# ${readmeData.title}
+  ## Table of contents
+  - [Installation](#installation)
+  - [Usage](#Usage)
+  - [Credits](#Credits)
+  - [License](#License)
+  - [Tests](#Tests)
+  ## Description
+  ${readmeData.projectDescription}
+  ## Installation
+  ${readmeData.projectInstallation}
+  ## Usage
+  ${readmeData.projectUsage}
+  ## Credits
+  ${readmeData.projectInstallation}
+  ## License
+  ${readmeData.License}
+  ## How to Contribute
+  ${readmeData.projectContribution}
+  ## Tests
+  ${readmeData.projectTest}
+  `;
 }
 
 module.exports = generateMarkdown;
