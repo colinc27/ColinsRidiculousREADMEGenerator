2bafbf9f783701b0edabf979e9c468333305f956
colinc27 <30709049+colinc27@users.noreply.github.com>
2022-04-03 11:07:10 -0700

    Commit First question

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,30 @@
 // TODO: Include packages needed for this application
-
+const fs = require('fs');
+const inquirer = require('inquirer');
+//const generatePage = require('./utils/generateMarkdown')
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = () => {
+    return inquirer.prompt([
+            {
+                type: 'input',
+                name: 'project',
+                message: 'What is the name of your project? (Required)',
+                validate: nameInput => {
+                  if (nameInput) {
+                    return true;
+                  } else {
+                    console.log('Please enter your name!');
+                    return false;
+                  }
+                }
+              }
+    ])}
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+//function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+//function init() {}
 
 // Function call to initialize app
-init();
+questions()
